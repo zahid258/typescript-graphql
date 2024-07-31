0a9b68edb9bfec0f9b2932c4c69c77514b95e192
Muhammad Tariq <53593504+tariqinfo304@users.noreply.github.com>
2024-07-31 16:29:00 +0500

    run the project without error

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,18 +1,39 @@
 import ConfigInterface from './ConfigInterface';
 
+
+// const config: ConfigInterface = {
+//   env: 'development',
+//   database: {
+//     type: 'sqlite' as const,
+//     cache: false,
+//     database: ':memory:',
+//     dropSchema: true,
+//     entities: ['src/entities/*.ts'],
+//     logger: 'advanced-console' as const,
+//     synchronize: true,
+//   },
+//   graphQLPath: '/graphql',
+//   resolvers: [`${__dirname}/../resolvers/**/* Resolver.ts`],
+// };
+
 const config: ConfigInterface = {
   env: 'development',
   database: {
-    type: 'sqlite' as const,
+    //@ts-ignore
+    type: 'mysql' as const,
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'test',
     cache: false,
-    database: ':memory:',
     dropSchema: true,
     entities: ['src/entities/*.ts'],
     logger: 'advanced-console' as const,
     synchronize: true,
   },
   graphQLPath: '/graphql',
-  resolvers: [`${__dirname}/../resolvers/**/*Resolver.ts`],
+  resolvers: [`${__dirname} /../resolvers/*Resolver.ts`],
 };
 
 export default config;
--- src/helpers/createSchema.ts
+++ src/helpers/createSchema.ts
@@ -1,10 +1,10 @@
 import { GraphQLSchema } from 'graphql';
 import { BuildSchemaOptions, buildSchemaSync } from 'type-graphql';
 import { Container } from 'typedi';
-
 export function createSchema(options?: Omit<BuildSchemaOptions, 'resolvers'>): GraphQLSchema {
+
   return buildSchemaSync({
-    resolvers: [`${__dirname}/../resolvers/**/*.resolver.ts`],
+    resolvers: [`${__dirname}/../resolvers/*Resolver.ts`],
     emitSchemaFile: true,
     container: Container,
     validate: true,
